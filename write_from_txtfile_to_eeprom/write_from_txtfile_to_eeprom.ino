1fc7f680a7f5360bbd758cc20ebe24c0190721ba
meguko <meguko2002@gmail.com>
2021-03-14 18:43:07 +0900

    コメントアウトを元に戻す。ESP32をデフォルトとした。(ESP8266も対応可

--- write_from_txtfile_to_eeprom/write_from_txtfile_to_eeprom.ino
+++ write_from_txtfile_to_eeprom/write_from_txtfile_to_eeprom.ino
@@ -1,5 +1,5 @@
-//#include <SPIFFS.h>  // ESP32
-#include "FS.h"  //ESP8266
+#include <SPIFFS.h>  // ESP32
+//#include "FS.h"  //ESP8266
 
 
 void setup(void) {
@@ -9,13 +9,13 @@ void setup(void) {
   SPIFFS.begin(); // ③SPIFFS開始
 
   String wrfile = "/ssid.txt"; // ④読み書きするファイル名を設定(fileは/data直下に置く）
-  //  String writeStr = "This is SPIFFS test!"; // ⑤書き込み文字列を設定
-  //  File fw = SPIFFS.open(wrfile.c_str(), "w");// ⑥ファイルを書き込みモードで開く
-  //  fw.println( writeStr ); // ⑦ファイルに書き込み
-  //  fw.close(); // ⑧ファイルを閉じる
-  //  Serial.print("SPIFFS Write:");  // ⑨書き込み完了をモニタに表示
-  //  Serial.println(writeStr);
-  //  delay(1000);
+    String writeStr = "This is SPIFFS test!"; // ⑤書き込み文字列を設定
+    File fw = SPIFFS.open(wrfile.c_str(), "w");// ⑥ファイルを書き込みモードで開く
+    fw.println( writeStr ); // ⑦ファイルに書き込み
+    fw.close(); // ⑧ファイルを閉じる
+    Serial.print("SPIFFS Write:");  // ⑨書き込み完了をモニタに表示
+    Serial.println(writeStr);
+    delay(1000);
 
   File fr = SPIFFS.open(wrfile.c_str(), "r");// ⑩ファイルを読み込みモードで開く
   while (1) {
